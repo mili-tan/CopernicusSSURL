ddbf861bf2cfcadfe570e1acdaeb532fd9bb1cf3
Milkey Tan <819774989@qq.com>
2018-03-29 13:27:38 +0800

    NameSpace

--- test/Program.cs
+++ test/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Copernicus.SSURL;
+﻿using Copernicus.ShadowsocksURi;
+using System;
 
 namespace test
 {
@@ -9,8 +9,8 @@ namespace test
         {
             while (true)
             {
-                Console.WriteLine(string.Join(",", SSURLTools.Parse(Console.ReadLine())));
-                Console.WriteLine(SSURLTools.SSEntity.encryptStr);
+                Console.WriteLine(string.Join(",", SSURi.Parse(Console.ReadLine())));
+                Console.WriteLine(SSEntity.encryptStr);
                 //Console.WriteLine(SSURL.Create("bf-cfb","233","2.33.33.33",2333));
                 Console.ReadLine();
             }
